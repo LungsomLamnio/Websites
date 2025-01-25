0c83fa10ec3fc0dcf46aa73e0772418001c73816
Lungsom Lamnio <lungsomlamnio@gmail.com>
2025-01-25 18:23:00 +0530

    restructure folder

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./frontend/src/Pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
